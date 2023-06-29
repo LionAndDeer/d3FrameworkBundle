a0dcdf7fde2077aa47c7aad092df506bbb745deb
Darius Drothen <darius.drothen@liondeer.de>
2023-06-29 16:59:27 +0200

    D3User and InterApp Changes

---
+++ src/D3/Proxy/HttpGateway/SubscribedAppsProxy.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Liondeer\Framework\D3\Proxy\HttpGateway;
+
+use Liondeer\Framework\Model\AppModel;
+use Liondeer\Framework\Model\SubscribedAppsModel;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class SubscribedAppsProxy
+{
+    public function __construct(
+        private HttpClientInterface $httpClient,
+    )
+    {
+    }
+
+    public function getSubscribedApps(string $tenantOrigin): false|SubscribedAppsModel
+    {
+        $counter = 10;
+        while ($counter > 0) {
+            try {
+                $response = $this->httpClient->request(
+                    'GET',
+                    "$tenantOrigin/httpgateway/conf/apps",
+                    [
+                        'headers' => [
+                            'accept' => 'application/json'
+                        ]
+                    ]
+                );
+                $content = $response->toArray();
+                $counter = -1;
+            } catch (\Exception $exception) {
+                // TODO: logging
+                sleep(2);
+                $counter -= 1;
+            }
+        }
+        if (empty($content)) {
+            return false;
+        }
+        $subscribedAppsModel = new SubscribedAppsModel();
+        foreach ($content['apps'] as $app) {
+            $subscribedAppsModel->addSubscribedApp($app['app']);
+        }
+        return $subscribedAppsModel;
+    }
+}
---
+++ src/Model/HttpGateway/AppModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Liondeer\Framework\Model\HttpGateway;
+
+class AppModel
+{
+    private string $name;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    /**
+     * @param string $name
+     * @return AppModel
+     */
+    public function setName(string $name): AppModel
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
