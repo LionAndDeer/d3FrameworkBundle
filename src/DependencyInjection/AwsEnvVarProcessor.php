1cf4ad24300386890e7f1be9a2e34ca5cbdba660
Leon Olbrück <leon.olbrueck@liondeer.de>
2023-10-16 12:26:53 +0200

    SYMF-7 AWSEnvVarProccessor added

---
+++ src/DependencyInjection/AwsEnvVarProcessor.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\EnvVarProcessorInterface;
+
+class AwsEnvVarProcessor implements EnvVarProcessorInterface
+{
+    public function getEnv(string $prefix, string $name, \Closure $getEnv): mixed
+    {
+        if ($getEnv('APP_ENV') == 'dev') {
+            return $getEnv($name);
+        }
+        try {
+            $json = json_decode(shell_exec('/opt/elasticbeanstalk/bin/get-config environment'), true, 512, JSON_THROW_ON_ERROR);
+            return $json[$name];
+        } catch (\Exception $exception) {
+            return $getEnv($name);
+        }
+
+    }
+
+    public static function getProvidedTypes(): array
+    {
+        return [
+            'secret' => 'string',
+            'string' => 'string'
+        ];
+    }
+}
